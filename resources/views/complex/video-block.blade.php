f9c943f28814fcb79194cd1cff489d29ab5060bd
katya <ekateryna.ne@gmail.com>
2016-08-14 19:08:40 +0300

    martinov-1

---
+++ resources/views/complex/video-block.blade.php
@@ -0,0 +1,12 @@
+<!-- video -->
+<section>
+    <h3 class="title-page">ЖК «София» от Мартынова: ваш собственный кусочек Европы</h3>
+
+    <div class="cell page-video">
+        <div class="video-wrapper">
+            <iframe width="890" height="540" src="https://www.youtube.com/embed/efqhudEJmFo" frameborder="0"
+                    allowfullscreen></iframe>
+        </div>
+    </div>
+</section>
+<!-- end video -->
--- resources/views/complex/video.blade.php
+++ resources/views/complex/video.blade.php
@@ -5,9 +5,16 @@
         <h1>{{ $complex->title }}</h1>
 
         <div class="cell9 p_r-10 cell-md">
-            <div class="cell type-flats m_b-20">
-                video
+            @include('complex.video-block')
+
+            @include('complex.video-carousel')
+
+            <div class="cell m_t-20 m_b-30">
+                @include('planstype.blue-info-block')
             </div>
+
+            @include('home.seo')
+
         </div>
 
         <div class="cell3 p_l-5 cell-md">
