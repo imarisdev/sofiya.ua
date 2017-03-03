6a9e76531664a08ae034da9c3fff4943f5342b02
root <kirill.krasyuk@gmail.com>
2017-03-03 14:40:10 -0500

    articles

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -7,4 +7,45 @@ use Watson\Rememberable\Rememberable;
 class BaseModel extends Model {
     use Rememberable;
 
+    public $extension = ".html";
+
+    /**
+     * Получить список колонок таблицы
+     *
+     * @return mixed
+     */
+    public function getTableColumns() {
+        return $this->getConnection()->getSchemaBuilder()->getColumnListing($this->getTable());
+    }
+
+    /**
+     * Полная ссылка на страницу
+     * @return string
+     */
+    public function getFullLink() {
+        return url('/') . $this->getLink();
+    }
+
+    /**
+     * Полная ссылка на категорию
+     * @return string
+     */
+    public function getFullPath() {
+        return url('/') . $this->getPath();
+    }
+
+    /**
+     * Срез текста
+     * @param $text
+     * @param int $length
+     * @return mixed
+     */
+    public function truncate($text, $length = 100) {
+        $length = abs((int)$length);
+        if(strlen($text) > $length) {
+            $text = strip_tags($text);
+            $text = preg_replace("/^(.{1,$length})(\s.*|$)/s", '\\1...', $text);
+        }
+        return($text);
+    }
 }
