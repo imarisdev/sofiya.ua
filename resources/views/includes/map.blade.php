3644eee1af00fe7555c3d52b21912b90a124c9c2
root <kirill.krasyuk@gmail.com>
2017-03-08 08:55:58 -0500

    map

--- resources/views/includes/map.blade.php
+++ resources/views/includes/map.blade.php
@@ -4,7 +4,9 @@
     @elseif($current_complex->id == 2)
         <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=CdTtu0siRdj8vJcS2F6WAAtcO_SDvAZS&amp;width=100%&amp;height=600&amp;lang=ru_RU&amp;sourceType=constructor&amp;scroll=false"></script>
     @elseif($current_complex->id == 3)
-
+        <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=CdTtu0siRdj8vJcS2F6WAAtcO_SDvAZS&amp;width=100%&amp;height=600&amp;lang=ru_RU&amp;sourceType=constructor&amp;scroll=false"></script>
+    @elseif($current_complex->id == 4)
+        <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=CdTtu0siRdj8vJcS2F6WAAtcO_SDvAZS&amp;width=100%&amp;height=600&amp;lang=ru_RU&amp;sourceType=constructor&amp;scroll=false"></script>
     @endif
 @else
     <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=oz1aGoXaBgGbL-4iblSnJ_QMDMUg57Nz&amp;width=100%&amp;height=600&amp;lang=ru_RU&amp;sourceType=constructor&amp;scroll=false"></script>
