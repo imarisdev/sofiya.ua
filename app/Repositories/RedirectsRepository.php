58fa69a0bcbaf9aa3c8e162434c8149087367665
root <kirill.krasyuk@gmail.com>
2017-03-08 06:51:15 -0500

    redirects

--- app/Repositories/RedirectsRepository.php
+++ app/Repositories/RedirectsRepository.php
@@ -106,4 +106,20 @@ class RedirectsRepository extends BaseRepository {
         return Response::json(['items' => $items->get()], 200);
     }
 
+    /**
+     * Удаление
+     * @param $house
+     */
+    public function destroy($article) {
+
+        try {
+
+            $article->delete();
+
+            return Response::json(['item' => true], 200);
+        } catch(\Exception $e) {
+            return Response::json(['error' => true, 'msg' => array($e->getMessage())], 400);
+        }
+
+    }
 }
