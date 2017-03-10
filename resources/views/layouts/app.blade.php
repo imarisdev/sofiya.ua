d3259a6b19b94e166d4b0f2d2fc5a64fe9740c24
root <kirill.krasyuk@gmail.com>
2017-03-10 12:47:46 -0500

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,9 +40,9 @@
             <footer>
                 @include('includes.footer')
             </footer>
-            <script src="//www.youtube.com/player_api"></script>
-	        <script src='//callbackhub.com/l/call.js'></script>
-            <script src="{{ elixir('js/common.js') }}"></script>
+            <script async src="//www.youtube.com/player_api"></script>
+	        <script async src='//callbackhub.com/l/call.js'></script>
+            <script async src="{{ elixir('js/common.js') }}"></script>
 
             <!-- Google Tag Manager (noscript) -->
             <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5Q4STN"
