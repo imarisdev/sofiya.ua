9625f044de7c434dcc663a2110b7965ce9ac387a
root <kirill.krasyuk@gmail.com>
2017-07-02 07:45:05 -0400

    update

--- resources/views/includes/socialdark.blade.php
+++ resources/views/includes/socialdark.blade.php
@@ -1,7 +1,7 @@
 <!--noindex-->
 <ul class="social">
     <li><a class="fb" target="_blank" href="https://www.facebook.com/sofiyacity/"><img src="/img/fixed-socials/fb.png" alt=""></a></li>
-    <li><a class="vk" target="_blank" href="https://vk.com/sofiyacity"><img src="/img/fixed-socials/vk.png" alt=""></a></li>
+    <!--li><a class="vk" target="_blank" href="https://vk.com/sofiyacity"><img src="/img/fixed-socials/vk.png" alt=""></a></li-->
     <li><a class="inst" target="_blank" href="https://www.instagram.com/sofiyaotmartynova/"><img src="/img/fixed-socials/inst.png" alt=""></a></li>
     <li><a class="yt" target="_blank" href="https://www.youtube.com/user/SofiyaCity"><img src="/img/fixed-socials/yt.png" alt=""></a></li>
     <li><a class="gp" target="_blank" href="https://plus.google.com/u/0/+%D0%96%D0%9A%D0%A1%D0%BE%D1%84%D0%B8%D1%8F"><img src="/img/fixed-socials/gp.png" alt=""></a></li>
